a36ab28b707ff5f5b4ceff31c577ad1d5c10fb60
Vyacheslav Gizov <vogizov@gmail.com>
2025-02-16 17:32:38 +0500

    add form and macros

---
+++ yacut/forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, URLField
+from wtforms.validators import DataRequired, Length, Optional, URL, Regexp
+
+
+LENGTH_MESSAGE = 'Допустимая длина ссылки: от %(min)d до %(max)d символов!'
+REQUIRED_MESSAGE = 'Это обязательное поле!'
+WRONG_URL_FORMAT = 'Недопустимый фомат URL!'
+WRONG_SHORT_URL = ('Допускаются: заглавные и прописные латинские '
+                   'буквы, цифры от 1 до 9!')
+
+
+class URLShortenerForm(FlaskForm):
+    original = StringField(
+        'Длинная ссылка',
+        validators=[
+            DataRequired(message=REQUIRED_MESSAGE),
+            Length(1, 256, message=LENGTH_MESSAGE),
+            URL(require_tld=False, message=WRONG_URL_FORMAT)
+        ]
+    )
+    short = StringField(
+        'Ваш вариант короткой ссылки',
+        validators=[
+            Optional(),
+            Length(1, 16, message=LENGTH_MESSAGE),
+            Regexp(r'(A-Z, a-z, 0-9)', message=WRONG_SHORT_URL)
+        ]
+    )
+    submit = SubmitField('Создать')
--- yacut/views.py
+++ yacut/views.py
@@ -2,14 +2,17 @@ from flask import render_template
 
 from . import app, db
 from .models import URLMap
+from .forms import URLShortenerForm
 
 
-@app.route('/')
-def index_view():
-    # url = URLMap(
-    #     original='dfkfhdkjhdfkfj',
-    #     short='dfkjhfd'
-    # )
-    # db.session.add(url)
-    # db.session.commit()
-    return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def shortener_view():  # возможно переименовать
+    form = URLShortenerForm()
+    if form.validate_on_submit():
+        url_map = URLMap(
+            original=form.original.data,
+            short=form.short.data
+        )
+        db.session.add(url_map)
+        db.session.commit()
+    return render_template('shortener.html', form=form)
